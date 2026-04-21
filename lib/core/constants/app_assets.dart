ac49da213cbffcd53be923d6962cc2d4e95db29c
KaiyouS <kserra@addu.edu.ph>
2026-04-22 04:51:22 +0800

    feat(core): add AppAssets class to centralize file paths

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,9 @@
+class AppAssets {
+  AppAssets._();
+
+  // Folders
+  static const String _branding = 'assets/branding';
+
+  // Specific Files
+  static const String appLogo = '$_branding/app_logo.png';
+}
